23d7bcf5c7afcad4251b4d06d65e304666afdc4d
dbetteb <dbetteb@yahoo.com>
2019-03-14 15:36:28 +0100

    comment

--- app.py
+++ app.py
@@ -5,7 +5,7 @@ from flask import Flask, render_template
 
 app = Flask(__name__, static_url_path='/static')
 
-
+# definition de la racine
 @app.route('/')
 def main():
     return render_template('index.html')
